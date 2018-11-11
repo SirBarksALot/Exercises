5a467a864f3e6155b1ebd5686147ff4849f2140c
Ulose <44412197+Ulose@users.noreply.github.com>
2018-11-11 14:18:06 +0100

    adding new ex

--- bubblesort.py
+++ bubblesort.py
@@ -7,27 +7,23 @@ tab = random.choices(population, weights=None, cum_weights=None, k=10)
 print(tab)
 
 
-def is_sorted(table):
-    for i in range(len(table)-1):
-        if table[i] <= table[i+1]:
-            pass
-        else:
-            return False
-    return True
-
-
 def sorting(table):
-    while not is_sorted(table):
+    while True:
         print(table)
-        print(is_sorted(table))
+        check_var = 0
         for i in range(len(table)-1):
             if table[i] > table[i+1]:
+                check_var = 1
                 table[i], table[i+1] = table[i+1], table[i]
             else:
                 pass
 
-    return table
+        if check_var == 0:
+            break
+        else:
+            pass
+
+    return 'Sorted table:\n{}'.format(table)
 
 
 print(sorting(tab))
-print('Sorted')
