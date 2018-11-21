54faf00a368d1dfa0231a52b400187b0c1e19cb2
Ulose <44412197+Ulose@users.noreply.github.com>
2018-11-21 10:18:11 +0100

    adding new ex

--- String lists - palindrome.py
+++ String lists - palindrome.py
@@ -1,13 +1,12 @@
-word = raw_input('Word to check for palindrome:  ')
+word = str(input('Word to check for palindrome:  '))
 
 var1 = 'palindrom'
 
-for i in range(0, len(word)):
+for i in range(0, len(word)//2):
     if word[i] == word[len(word)-1-i]:
         pass
     else:
-        var1 = 'not palindrom'
+        var1 = 'not palindrome'
         break
     
-print word + ' is ' + var1 + '!'
-        
+print(word + ' is ' + var1 + '!')
