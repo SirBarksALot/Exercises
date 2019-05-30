20eec1ae45906ccb941deabc923aa6dd389d8b12
filip.lezuch <filiplez18@gmail.com>
2019-05-30 12:27:41 +0200

    upgraded first occurence exercise

--- String reoccurence.py
+++ String reoccurence.py
@@ -1,15 +1,13 @@
-word = str(input('Pick a word: '))
-global i
+def finding_first_occurence(word):
+    temp_table = []
+    for letter in word:
+        if letter in temp_table:
+            return letter
+        else:
+            temp_table.append(letter)
 
-
-def finding_recurence(word):
-    for i in range(len(word)):
-        for ii in range(i+1, len(word)):
-            if word[i] == word[ii]:
-                return 'First letter recurrence: {}'.format(word[i])
-            else:
-                pass
     return 'Recurrence not found!'
 
 
-print(finding_recurence(word))
+word_to_search = str(input('Pick a word: '))
+print(finding_first_occurence(word_to_search))
