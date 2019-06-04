45aa7b25a81f5efabb9e2cee2e044d8a3d5fd3ce
filip.lezuch <filiplez18@gmail.com>
2019-06-04 10:17:20 +0200

    fixed

--- Subarray with given sum.py
+++ Subarray with given sum.py
@@ -1,26 +1,27 @@
 # Given an unsorted array A of size N of non-negative integers,
 # find a continuous sub-array which adds to a given number S.
 
-A = [2, 1, 3, 0, 5, 8, 9, 4, 5, 6, 7, 3, 2, 5, 1, 8]
+A = [1, 2, 7, 4, 5, 6, 7, 8, 9, 10]
 N = len(A)
-S = 10
+S = 15
 
 
 def find_sub_array(array, s):
-    index = 0
+    s_holder = s
 
-    for i in range(len(array)):
-        print(s, index)
-        s = s - array[i]
-        print(s)
-        if s == 0:
-            return array[index:i+1]
-        elif s > 0:
-            index += 1
-        else:
-            index = i
-            s = 10
-        print(s, index)
+    for j in range(len(array)):
+        index = j
+        for i in range(j, len(array)):
+            s = s - array[i]
+            if s == 0:
+                return array[index:i+1]
+            elif s < 0:
+                s = s_holder
+                break
+            else:
+                pass
+
+            print(s, index)
 
     return 'No sublist matching given criteria!'
 
