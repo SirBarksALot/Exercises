622bc7c870f6ea5935ed96375592f2567e37418d
filip.lezuch <filiplez18@gmail.com>
2019-06-11 16:02:14 +0200

    add missing in a list

--- Missing number in array.py
+++ Missing number in array.py
@@ -1,11 +1,12 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 10]
 arr_len = 10
 
 
 def find_missing():
-    for i in range(0, arr_len):
-        if i == (arr_len-1) or i + 1 != arr[i]:
+    for i in range(0, arr_len-1):
+        if i + 1 != arr[i]:
             return i+1
+    return arr_len
 
 
 print(find_missing())
