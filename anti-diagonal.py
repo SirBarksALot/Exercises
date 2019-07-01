f7d1dead58719aae5af1ec7d186d938054ecf45f
SirBarksALot <44412197+SirBarksALot@users.noreply.github.com>
2019-07-01 18:09:31 +0200

    Update anti-diagonal.py

--- anti-diagonal.py
+++ anti-diagonal.py
@@ -1,9 +1,15 @@
 # Give a N*N square matrix, return an array of its anti-diagonals.
 
 
-arr = [[1, 2, 3],
-       [4, 5, 6],
-       [7, 8, 9]]
+#arr = [[1, 2, 3],
+#       [4, 5, 6],
+#       [7, 8, 9]]
+
+arr = [[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [9, 10, 11, 12],
+       [13, 14, 15, 16]]
+
 
 for row in arr:
     print(row)
@@ -16,17 +22,16 @@ def diagonal_printing():
     while k < (len(arr)):
         k += 1
         for i in range(k):
-            temp_arr.append(arr[i][k - 1 - i])
+            temp_arr.append(arr[k - 1 - i][i])
 
         print(temp_arr)
         temp_arr = []
 
-    k = 0
-    while k < (len(arr))-1:
-        k += 1
-        print(k)
-        for i in range(k, len(arr)):
-            temp_arr.append(arr[i][len(arr) - i])
+    s = 0
+    while s < len(arr)-1:
+        s += 1
+        for i in range(len(arr)-s):
+            temp_arr.append(arr[len(arr) - i - 1][s+i])
 
         print(temp_arr)
         temp_arr = []
