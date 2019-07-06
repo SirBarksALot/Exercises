770404c00e813a298abd582c174d0bd05a6dcd43
Ulose <44412197+Ulose@users.noreply.github.com>
2019-07-06 18:29:01 +0200

    up

--- Binary tree.py
+++ Binary tree.py
@@ -4,9 +4,37 @@ class Node:
         self.left = None
         self.right = None
 
+    def insert(self, data):
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        else:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
+
     def print(self):
-        print('{}--{}--{}'.format(self.left, self.data, self.right))
+        if self.left is None:
+            if self.right is None:
+                print(f'{None}--{self.data}--{None}')
+            else:
+                print(f'{None}--{self.data}--{self.right.print()}')
+
+        else:
+            if self.right is None:
+                print(f'{self.left.print()}--{self.data}--{None}')
+            else:
+                print(f'{self.left.print()}--{self.data}--{self.right.print()}')
+        return self.data
 
 
 root = Node(15)
+root.insert(22)
+root.insert(2)
+root.insert(7)
+root.insert(1)
+root.insert(16)
 root.print()
